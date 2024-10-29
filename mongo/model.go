2d02b7a87aa5c6705bfce00c7bd79fcd2afea5d2
Eason.Chiu <zhida.zhao@atzuche.cn>
2024-10-29 12:58:46 +0800

    feat: fix base model

--- mongo/model.go
+++ mongo/model.go
@@ -1,20 +1,22 @@
 package mongo
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // BaseModel `bson:",inline"`
 
 type BaseModel struct {
-	ID        primitive.ObjectID `json:"id"        bson:"_id,omitempty"`
-	CreatedAt int64              `json:"created_at" bson:"created_at"`
-	UpdatedAt int64              `json:"updated_at" bson:"updated_at"`
-	DeletedAt *int64             `json:"-"         bson:"deleted_at,omitempty"`
-	IsDeleted bool               `json:"-"         bson:"is_deleted"`
+	ID        primitive.ObjectID `json:"id"         bson:"_id,omitempty"`
+	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+	DeletedAt *time.Time         `json:"-"          bson:"deleted_at,omitempty"`
+	IsDeleted bool               `json:"-"          bson:"is_deleted"`
 }
 
 func (b *BaseModel) BeforeCreate() {
-	b.CreatedAt = nowTimestamp()
+	b.CreatedAt = time.Now()
 	b.UpdatedAt = b.CreatedAt
 }
